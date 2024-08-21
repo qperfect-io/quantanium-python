f9d00cff8b8b6c6ef7fbf84f65cd7821cc89ec7f
anleo2023 <anna.leonteva@qperfect.io>
2024-08-21 12:03:54 +0200

    doc: add Sphinx style comments for examples documentation

--- examples/example_circuit.py
+++ examples/example_circuit.py
@@ -4,22 +4,56 @@ from mimiqcircuits import *
 from mimiqcircuits import Circuit as MimiqCircuit
 from mimiqlink import MimiqConnection as BaseMimiqConnection
 
-
 def main():
+    """
+    Main function to demonstrate the usage of the Quantanium class with MIMIQ API.
 
+    This function initializes the Quantanium processor in local mode, constructs a simple MimiqCircuit and then executes the circuit. The results of the execution
+    are printed.
+
+    Steps:
+    1. Initializes a `Quantanium` processor with local execution.
+    2. Creates a Mimiq circuit with Hadamard (H) and CNOT (CX) gates.
+    3. Executes the circuit locally with specified sampling and seed.
+    4. Prints the results of the execution.
+
+    Raises:
+        Exception: If any errors occur during the execution of the circuit or conversion processes.
+    
+    Usage Example:
+        To run this script, use the following command in your terminal:
+
+        ```bash
+        poetry run python examples/example_circuit.py 
+        ```
+
+    This will initialize a Quantanium processor, create a simple quantum circuit with a Hadamard gate on 
+    qubit 0 and a CNOT gate on control qubit 0 and target qubits 2,3,4 and then execute this circuit.
+    The results will be printed to the console.
+    
+    """
     try:
         # Initialize the processor with local execution
         processor = Quantanium()
+        
+        # Create a Mimiq circuit and add gates
         mimiq_circuit = MimiqCircuit()
-        mimiq_circuit.push(GateH(),0)
-        mimiq_circuit.push(GateCX(), 0, range(2,4))
+        mimiq_circuit.push(GateH(), 0)  # Apply Hadamard gate to qubit 0
+        mimiq_circuit.push(GateCX(), 0, range(2, 4))  # Apply CNOT gate 
+        
+        # Print the constructed Mimiq circuit
         print(mimiq_circuit)
-        qua_circuit = processor.convert_mimiq_to_qua_circuit(mimiq_circuit)
+        
+        # Execute the circuit locally with 100 samples and a fixed seed of 1
         results = processor.execute(mimiq_circuit, nsamples=100, seed=1)
+        
+        # Print the results of the execution
         print(results)
     except Exception as e:
+        # Catch and print any exceptions that occur
         print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
     main()
+
 
--- examples/example_qasm.py
+++ examples/example_qasm.py
@@ -1,8 +1,33 @@
 import argparse
 from quantaniumpy.Quantanium import Quantanium
 
-
 def main():
+    """
+    Main function to process a circuit from QASM file using the Quantanium class of quantaniumpy package.
+
+    This function performs the following steps:
+    1. Parses command-line arguments to get the path to the QASM file.
+    2. Initializes the Quantanium processor in local mode.
+    3. Converts the provided QASM file into a QUA circuit.
+    4. Executes the circuit locally with a specified number of samples and a fixed seed.
+    5. Prints the results of the execution.
+
+    Args:
+        qasm_file (str): The path to the QASM file provided via command-line arguments.
+
+    Raises:
+        Exception: If any errors occur during the QASM conversion or circuit execution.
+    
+    Usage Example:
+        To run this script, use the following command in your terminal:
+        
+        ```bash
+        $ poetry run python examples/example_qasm.py path/to/your/qasm/your_circuit.qasm
+        ```
+
+        
+        This will process the QASM file and print the results of the circuit execution.
+    """
     parser = argparse.ArgumentParser(description="Process a QASM file with Quantanium.")
     parser.add_argument('qasm_file', type=str, help='The path to the QASM file')
 
@@ -11,15 +36,20 @@ def main():
     try:
         # Initialize the processor with local execution
         processor = Quantanium()
-        # Convert QASM file to qua::Circuit
+        
+        # Convert QASM file to QUA circuit
         qua_circuit = processor.convert_qasm_to_qua_circuit(args.qasm_file)
-        # Execute the circuit
+        
+        # Execute the circuit locally with 100 samples and a fixed seed of 1
         results = processor.execute(qua_circuit, nsamples=100, seed=1)
-        # print the obteined results
+        
+        # Print the obtained results
         print(results)
     except Exception as e:
+        # Catch and print any exceptions that occur
         print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
     main()
+
 
