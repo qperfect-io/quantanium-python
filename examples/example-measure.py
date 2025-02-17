6fe0da15e6654c72c6c58d14622e8c22e89a77b3
Maxime-pixel <55755681+Maxime-pixel@users.noreply.github.com>
2025-02-17 10:52:48 +0100

    Devel (#7)
    * fix: package name
    
    * docs: add LICENCE file
    
    * docs: fix file name
    
    * fix: use password token for pypi
    
    * fix : updates examples and CMAKE
    
    * fix: module name

--- examples/example-measure.py
+++ examples/example-measure.py
@@ -1,22 +1,20 @@
 
 import numpy as np
-from quantaniumpy.Quantanium import Quantanium
+from quantanium.Quantanium import Quantanium
 from mimiqcircuits import *
 
 
 processor = Quantanium()
 
 circuit = Circuit()
-circuit.push(GateX(), range(1,5))
- 
-circuit.push(MeasureX(), 0,0)
-circuit.push(MeasureY(), 1,1)
-circuit.push(MeasureZ(), 2,1)
-circuit.push(MeasureResetX(), 3,1)
-circuit.push(MeasureResetY(), 0,1)
-circuit.push(MeasureResetZ(), 1,1)
+circuit.push(GateX(), range(1, 5))
+
+circuit.push(MeasureX(), 0, 0)
+circuit.push(MeasureY(), 1, 1)
+circuit.push(MeasureZ(), 2, 1)
+circuit.push(MeasureResetX(), 3, 1)
+circuit.push(MeasureResetY(), 0, 1)
+circuit.push(MeasureResetZ(), 1, 1)
 circuit.push(Reset(), 2)
 r = processor.execute(circuit, nsamples=100)
 print(r)
-
-
